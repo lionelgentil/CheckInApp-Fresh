0cc469a2b93ad01601dd6d6ae0158cf79ee44b49
Lionel Gentil <lgentil@apple.com>
2025-08-15 07:39:02 -0700

    Commit 2.1.1 woth Postgres

--- api.php
+++ api.php
@@ -53,6 +53,7 @@ try {
         case 'health':
             echo json_encode([
                 'status' => 'OK',
+                'version' => '2.1.1',
                 'timestamp' => date('c'),
                 'database' => $dbType === 'postgresql' ? 'PostgreSQL' : 'SQLite',
                 'php_version' => PHP_VERSION,
