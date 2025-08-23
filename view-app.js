e3a2d6c898c81dd3076171617e1cbee3d8e6e53e
Lionel Gentil <lgentil@apple.com>
2025-08-22 18:56:05 -0700

    Commit 3.0.0 with Standings and Card Tracker

--- view-app.js
+++ view-app.js
@@ -4,7 +4,7 @@
  */
 
 // Version constant - update this single location to change version everywhere
-const APP_VERSION = '2.16.30';
+const APP_VERSION = '3.0.0';
 
 class CheckInViewApp {
     constructor() {
