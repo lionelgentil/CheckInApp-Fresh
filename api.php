0a67a7458931fa67bb6a737ff140fbac1a32af83
Lionel Gentil <lgentil@apple.com>
2025-09-08 17:52:05 -0700

    Commit 6.6.0

--- api.php
+++ api.php
@@ -8,7 +8,7 @@
 session_start();
 
 // Version constant - update this single location to change version everywhere
-const APP_VERSION = '5.5.4';
+const APP_VERSION = '6.0.0';
 
 // Authentication configuration
 const ADMIN_PASSWORD = 'checkin2024'; // Change this to your desired password
